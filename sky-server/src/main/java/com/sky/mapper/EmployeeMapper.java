c4c3251704208aa82f484757f65e8edc2328dcd6
54DSA <suyichen0124@gmail.com>
2024-12-27 18:19:36 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -14,5 +15,6 @@ public interface EmployeeMapper {
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
-
+//    @Insert("insert into" employee)
+    void insert(Employee employee);
 }
